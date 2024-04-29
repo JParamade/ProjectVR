a5cb1169283df32f7a92cc283ba2e0629abd85eb
JParamade <114541314+JParamade@users.noreply.github.com>
2024-04-29 19:42:56 +0200

    Cube Magnet Feature Prototype

--- ProjectVR/Assets/_Scripts/BlockBehaviour.cs
+++ ProjectVR/Assets/_Scripts/BlockBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BlockBehaviour : MonoBehaviour
 {
@@ -8,17 +9,23 @@ public class BlockBehaviour : MonoBehaviour
 
     private void Update()
     {
-       RaycastHit hit;
-       //if (Physics.Raycast(new Vector3(0, transform.position.y + (transform.localScale.y / 2), 0), transform.up, out hit, raycastDistance, layerMask))       
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position + new Vector3(transform.localScale.x / 2, 0, 0), transform.right, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(transform.localScale.x, 0, 0); }
+        if (Physics.Raycast(transform.position + new Vector3(-transform.localScale.x / 2, 0, 0), -transform.right, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(transform.localScale.x, 0, 0); }
+        if (Physics.Raycast(transform.position + new Vector3(0, transform.localScale.y / 2, 0), transform.up, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(0, transform.localScale.y, 0); }
+        if (Physics.Raycast(transform.position + new Vector3(0, -transform.localScale.y / 2, 0), -transform.up, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(0, transform.localScale.y, 0); }
+        if (Physics.Raycast(transform.position + new Vector3(0, 0, transform.localScale.z / 2), transform.forward, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(0, 0, transform.localScale.z); }
+        if (Physics.Raycast(transform.position + new Vector3(0, 0, -transform.localScale.z / 2), -transform.forward, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(0, 0, transform.localScale.z); }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawRay(new Vector3(0, transform.position.y + (transform.localScale.y / 2), 0), transform.up * raycastDistance);
-        Gizmos.DrawRay(new Vector3(0, -(transform.position.y + (transform.localScale.y / 2)), 0), -transform.up * raycastDistance);
-        Gizmos.DrawRay(new Vector3(0, transform.position.x + (transform.localScale.y / 2), 0), transform.right * raycastDistance);
-        Gizmos.DrawRay(new Vector3(0, -(transform.position.x + (transform.localScale.y / 2)), 0), -transform.right * raycastDistance);
-        Gizmos.DrawRay(new Vector3(0, transform.position.z + (transform.localScale.y / 2), 0), transform.forward * raycastDistance);
-        Gizmos.DrawRay(new Vector3(0, -(transform.position.z + (transform.localScale.y / 2)), 0), -transform.forward * raycastDistance);
+        Gizmos.DrawRay(transform.position + new Vector3(transform.localScale.x / 2, 0, 0), transform.right * raycastDistance);
+        Gizmos.DrawRay(transform.position + new Vector3(-transform.localScale.x / 2, 0, 0), -transform.right * raycastDistance);
+        Gizmos.DrawRay(transform.position + new Vector3(0, transform.localScale.y / 2, 0), transform.up * raycastDistance);
+        Gizmos.DrawRay(transform.position + new Vector3(0, -transform.localScale.y / 2, 0), -transform.up * raycastDistance);
+        Gizmos.DrawRay(transform.position + new Vector3(0, 0, transform.localScale.z / 2), transform.forward * raycastDistance);
+        Gizmos.DrawRay(transform.position + new Vector3(0, 0, -transform.localScale.z / 2), -transform.forward * raycastDistance);
     }
 }
