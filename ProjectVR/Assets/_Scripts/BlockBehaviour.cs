412093dc9ae159c9cd93bf5add5e68db8b00789c
Jaime Páramo <jaime.paramo@outlook.com>
2024-04-30 16:32:44 +0200

    Cube Magnet Feature Update
    New cube magnet system started (not finished).

--- ProjectVR/Assets/_Scripts/BlockBehaviour.cs
+++ ProjectVR/Assets/_Scripts/BlockBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,15 +9,19 @@ public class BlockBehaviour : MonoBehaviour
     [SerializeField] private float raycastDistance;
 
     private void Update()
-    {
+    {   
+        CheckRaycast();
+    }
+
+    private void CheckRaycast() {
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position + new Vector3(transform.localScale.x / 2, 0, 0), transform.right, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(transform.localScale.x, 0, 0); }
-        if (Physics.Raycast(transform.position + new Vector3(-transform.localScale.x / 2, 0, 0), -transform.right, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(transform.localScale.x, 0, 0); }
-        if (Physics.Raycast(transform.position + new Vector3(0, transform.localScale.y / 2, 0), transform.up, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(0, transform.localScale.y, 0); }
-        if (Physics.Raycast(transform.position + new Vector3(0, -transform.localScale.y / 2, 0), -transform.up, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(0, transform.localScale.y, 0); }
-        if (Physics.Raycast(transform.position + new Vector3(0, 0, transform.localScale.z / 2), transform.forward, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(0, 0, transform.localScale.z); }
-        if (Physics.Raycast(transform.position + new Vector3(0, 0, -transform.localScale.z / 2), -transform.forward, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(0, 0, transform.localScale.z); }
+        if (Physics.Raycast(transform.position + new Vector3(transform.localScale.x / 2, 0, 0), transform.right, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(transform.localScale.x, 0, 0); hit.collider.gameObject.transform.SetParent(gameObject.transform); }
+        if (Physics.Raycast(transform.position + new Vector3(-transform.localScale.x / 2, 0, 0), -transform.right, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(transform.localScale.x, 0, 0); hit.collider.gameObject.transform.SetParent(gameObject.transform); }
+        if (Physics.Raycast(transform.position + new Vector3(0, transform.localScale.y / 2, 0), transform.up, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(0, transform.localScale.y, 0); hit.collider.gameObject.transform.SetParent(gameObject.transform); }
+        if (Physics.Raycast(transform.position + new Vector3(0, -transform.localScale.y / 2, 0), -transform.up, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(0, transform.localScale.y, 0); hit.collider.gameObject.transform.SetParent(gameObject.transform); }
+        if (Physics.Raycast(transform.position + new Vector3(0, 0, transform.localScale.z / 2), transform.forward, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position + new Vector3(0, 0, transform.localScale.z); hit.collider.gameObject.transform.SetParent(gameObject.transform); }
+        if (Physics.Raycast(transform.position + new Vector3(0, 0, -transform.localScale.z / 2), -transform.forward, out hit, raycastDistance)) { hit.collider.gameObject.transform.position = transform.position - new Vector3(0, 0, transform.localScale.z); hit.collider.gameObject.transform.SetParent(gameObject.transform); }
     }
 
     private void OnDrawGizmos()
---
+++ ProjectVR/Assets/_Scripts/CubeTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTrigger : MonoBehaviour
+{
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+}
