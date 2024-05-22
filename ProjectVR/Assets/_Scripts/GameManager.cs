9269fa41274b32b2e49b1d1118fc84ebb9545094
Jaime Páramo <jaime.paramo@outlook.com>
2024-05-22 19:05:19 +0200

    First Puzzle Prototype

---
+++ ProjectVR/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] List<MeshRenderer> lightMR = new List<MeshRenderer>();
+    
+    private void Update() {
+        
+    }
+}
