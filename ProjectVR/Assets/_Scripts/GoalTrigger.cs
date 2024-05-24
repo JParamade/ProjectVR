789b255dd14fc1f5b41c5eb927ef81f81b6ffe77
JParamade <114541314+JParamade@users.noreply.github.com>
2024-05-24 03:07:44 +0200

    Script Minor Adjustments

--- ProjectVR/Assets/_Scripts/GoalTrigger.cs
+++ ProjectVR/Assets/_Scripts/GoalTrigger.cs
@@ -6,17 +6,17 @@ public class GoalTrigger : MonoBehaviour
 {
     [SerializeField] PuzzleFace puzzleFace;
     [SerializeField] Material goalMaterial;
-
+ 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.GetComponent<SphereCollider>() && !other.gameObject.GetComponent<Rigidbody>().isKinematic)
         {
             other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-
+ 
             if (puzzleFace == PuzzleFace.FRONT) { CubePuzzleController.Instance.lights[0].material = goalMaterial; CubePuzzleController.Instance.puzzlesCompleted++; }
             else if (puzzleFace == PuzzleFace.BACK) { CubePuzzleController.Instance.lights[1].material = goalMaterial; CubePuzzleController.Instance.puzzlesCompleted++; }
             else if (puzzleFace == PuzzleFace.RIGHT) { CubePuzzleController.Instance.lights[2].material = goalMaterial; CubePuzzleController.Instance.puzzlesCompleted++; }
             else { CubePuzzleController.Instance.lights[3].material = goalMaterial; CubePuzzleController.Instance.puzzlesCompleted++; }
-
+ 
             if (CubePuzzleController.Instance.puzzlesCompleted >= 4) { CubePuzzleController.Instance.OnPuzzleCompleted(); }
         }
     }
